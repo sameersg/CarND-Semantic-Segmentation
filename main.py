36b7d9acc3d04e3d9fd955ee20026e18dd7635b5
sameersg <sameer@goswami.de>
2017-11-18 14:00:39 +0100

    working

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
+
     return None, None, None, None, None
 tests.test_load_vgg(load_vgg, tf)
 
